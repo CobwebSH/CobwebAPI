fa581ece0e7b35a8292e0923aecbcde03d69f1bf
JФSԐPЊҤЇ ҠЯДԞФЩSҜЇ <53848716+SlavicMan@users.noreply.github.com>
2022-04-18 00:34:14 +0930

    adding stuffs
    weapon spawner probably doesn't work
    I don't remember if the getVersusWeapons() works either anymore

--- CobwebAPI/API/Weapons.cs
+++ CobwebAPI/API/Weapons.cs
@@ -43,4 +43,34 @@ public class Weapons
         weapon.type = weaponType;
     }
     
+    private List<VersusWeapon> getVersusWeapons()
+    {
+        bool flag = GameSettings.Instance == null;
+        List<VersusWeapon> result;
+        if (flag)
+        {
+            result = null;
+        }
+        else
+        {
+            MelonLogger.Msg(GameSettings.Instance.versusWeapons.Count);
+            List<VersusWeapon> list = GameSettings.Instance.AvailableVersusWeapons();
+            this.runOnce = false;
+            result = list;
+        }
+        return result;
+    }
+    
+    private void spawnWeapon(int selectedWeapon)
+    {
+        GameObject gameObject = GameObject.FindGameObjectWithTag("PlayerRigidbody");
+        Vector2 a = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        bool flag = !Physics2D.Raycast(a + new Vector2(0f, 10f), gameObject.transform.up, 0.1f, GameController.instance.worldLayers);
+        bool flag2 = flag;
+        if (flag2)
+        {
+            UnityEngine.Object.Instantiate<GameObject>(this.getVersusWeapons()[selectedWeapon].weapon, gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+    
 }
