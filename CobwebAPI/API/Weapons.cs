4410f49bea6594e0308dea8faaa02db6a14c3e1a
Daemon <DaemonBeast@outlook.com>
2022-04-19 16:27:31 +0100

    Upgraded tooling

--- CobwebAPI/API/Weapons.cs
+++ CobwebAPI/API/Weapons.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using HarmonyLib;
 
 namespace CobwebAPI.API;
--- CobwebAPI/Main.cs
+++ CobwebAPI/Main.cs
@@ -2,24 +2,22 @@
 using BepInEx.Logging;
 using HarmonyLib;
 
-namespace CobwebAPI
-{
-    [BepInPlugin(ModGUID, ModName, ModVersion)]
-    public class Main : BaseUnityPlugin
-    {
-        public const string ModName = "CobwebAPI";
-        public const string ModAuthor  = "CobwebSH";
-        public const string ModGUID = "com.cobwebsh.cobwebapi";
-        public const string ModVersion = "1.0.0";
-        internal static ManualLogSource Log;
-        internal Harmony Harmony;
-        internal void Awake()
-        {
-            Harmony = new Harmony(ModGUID);
-            Log = Logger;
+namespace CobwebAPI;
 
-            Harmony.PatchAll();
-            Logger.LogInfo($"{ModName} successfully loaded! Made by {ModAuthor}");
-        }
+[BepInAutoPlugin("com.cobwebsh.cobwebapi", "CobwebAPI")]
+public partial class Main : BaseUnityPlugin
+{
+    public const string Author  = "CobwebSH";
+
+    internal static ManualLogSource Log = null!;
+    
+    internal Harmony Harmony { get; } = new(Id);
+    
+    internal void Awake()
+    {
+        Log = Logger;
+
+        Harmony.PatchAll();
+        Logger.LogInfo($"{Name} successfully loaded! Made by {Author}");
     }
 }
