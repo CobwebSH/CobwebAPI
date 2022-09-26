d395ec970639abafc0b9bc5116cdffc3a38f4ab8
Ecorous <exo@frogastudios.com>
2022-09-26 19:23:47 +0100

    Format Weapons

--- CobwebAPI/API/Weapons.cs
+++ CobwebAPI/API/Weapons.cs
@@ -7,11 +7,11 @@ namespace CobwebAPI.API;
 
 public class Weapons
 {
-    
     [HarmonyPatch(typeof(WeaponManager))]
     public class WeaponManagerPatch
     {
         public static Weapon? EquippedWeapon { get; internal set; }
+
         [HarmonyPatch("EquipWeapon")]
         [HarmonyPostfix]
         internal static void EquipWeaponPostfix(ref Weapon weapon)
@@ -22,6 +22,7 @@ public class Weapons
                 weapon = EquippedWeapon;
             }
         }
+
         [HarmonyPatch("EquipWeapon")]
         [HarmonyPrefix]
         internal static void EquipWeaponPrefix(ref Weapon weapon)
@@ -31,6 +32,7 @@ public class Weapons
                 weapon = EquippedWeapon;
             }
         }
+
         [HarmonyPatch("UnEquipWeapon")]
         [HarmonyPostfix]
         internal static void UnEquipWeaponPostfix()
@@ -38,15 +40,18 @@ public class Weapons
             EquippedWeapon = null;
         }
     }
+
     public static Weapon? GetEquippedWeapon()
     {
         return WeaponManagerPatch.EquippedWeapon;
     }
-     // * To be done
+
+    // * To be done
     public static void SetEquippedWeapon(Weapon weapon)
     {
         WeaponManagerPatch.EquippedWeapon = weapon;
     }
+
     // Add weapon
     public static Weapon CreateWeapon(int ammo, List<Weapon.WeaponType> weaponType, string label)
     {
@@ -61,10 +66,10 @@ public class Weapons
         return weapon;
     }
 
-    
-     //* To be done
-     
-     private List<SpawnableWeapon> getVersusWeapons()
+
+    //* To be done
+
+    private List<SpawnableWeapon> getVersusWeapons()
     {
         bool flag = GameSettings.Instance == null;
         List<SpawnableWeapon> result;
@@ -77,14 +82,16 @@ public class Weapons
             List<SpawnableWeapon> list = GameSettings.Instance.AvailableVersusWeapons();
             result = list;
         }
+
         return result;
     }
-    
+
     private void spawnWeapon(int selectedWeapon)
     {
         GameObject gameObject = GameObject.FindGameObjectWithTag("PlayerRigidbody");
         Vector2 a = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
-        bool flag = !Physics2D.Raycast(a + new Vector2(0f, 10f), gameObject.transform.up, 0.1f, GameController.instance.worldLayers);
+        bool flag = !Physics2D.Raycast(a + new Vector2(0f, 10f), gameObject.transform.up, 0.1f,
+            GameController.instance.worldLayers);
         bool flag2 = flag;
         if (flag2)
         {
